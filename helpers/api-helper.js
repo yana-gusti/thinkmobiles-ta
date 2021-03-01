9ce5546287aefa842bb32b573498f7cd69187a8a
yana-gusti <yana.gusti@gmail.com>
2021-03-01 17:45:36 +0200

    Fix login

--- helpers/api-helper.js
+++ helpers/api-helper.js
@@ -2,14 +2,14 @@ const request = require('request');
 
 module.exports = thisModule = {
 
-    sendRequest: (method = 'POST', uri, body, headers, additionalOptions) =>{
+    sendRequest: (method = 'POST', uri, body, headers, additionalOptions) => {
         return new Promise((resolve, reject) => {
             const options = {
                 method,
                 uri,
                 body,
                 headers: mapHeaders(headers),
-                agentOptions: { rejectUnauthorized: false },
+                agentOptions: {rejectUnauthorized: false},
                 ...additionalOptions
             };
 
@@ -24,26 +24,29 @@ module.exports = thisModule = {
         });
     },
 
-    login: async function(email, password){
-    const data = {
-             email: email,
-             password: password,
-             rememberMe: false
-         };
-         const url = 'https://thinkmobiles.com/api/auth/sign-in/'
+    login: async function (email, password) {
+        const data = {
+            email: email,
+            password: password,
+            rememberMe: false
+        };
+        const url = 'https://thinkmobiles.com/api/auth/sign-in/'
 
-         let headers = new Map();
-         headers.set("Content-Type", "application/json");
-         headers.set("User-Agent", "Thinkmobiles-qa");
-         headers.set("X-Testing-Token", "fsdjdsfJKdfhs723kldsfjkls23890klsdfkljhhvxcLKJsdf98732lkkmsfdjhksf8");
+        let headers = new Map();
+        headers.set("Content-Type", "application/json");
+        headers.set("User-Agent", "Thinkmobiles-qa");
+        headers.set("X-Testing-Token", "fsdjdsfJKdfhs723kldsfjkls23890klsdfkljhhvxcLKJsdf98732lkkmsfdjhksf8");
 
-         response = await apiHelper.sendRequest("POST", url, JSON.stringify(data), headers);
-         console.log(response.request.body);
-         console.log(response.request.headers);
-         console.log(response.body);
-         console.log(response.statusCode);
-         console.log(response.body.id);
-         return response.body.id;
+        let response = await thisModule.sendRequest("POST", url, JSON.stringify(data), headers);
+        let body = JSON.parse(response.body);
+        // console.log(response.request.body);
+        // console.log(response.request.headers);
+        // console.log(response.body);
+        // console.log(response.statusCode);
+        // console.log(response.body);
+        let user = body['user'];
+        let id = user.id;
+        return id;
     }
 };
 
