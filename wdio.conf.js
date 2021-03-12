464f583f7c99e6b2e804e4a42388abc43817a66e
Eugene1987uzh <eugene_andr@yahoo.com>
2021-03-12 14:19:36 +0200

    api-steps deleted

--- wdio.conf.js
+++ wdio.conf.js
@@ -60,8 +60,8 @@ exports.config = {
         // <string[]> (file/dir) require files before executing features
 
         require: [
-          //  './features/step-definitions/api-steps-a.js',
-            './features/step-definitions/regSteps.js',
+            './features/step-definitions/api-steps-a.js',
+          //  './features/step-definitions/regSteps.js',
         ],
         backtrace: false,
         // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
